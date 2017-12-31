2189a6e4704c05224e71bf770347573f3e05a2fd
hitesh-2457 <hitesh.2457@outlook.com>
2017-12-31 17:30:40 -0600

    changed the site to SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { appRoutingModule } from './app.routing';
-
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { HomeComponent } from './home/home.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SkillsComponent } from './about/skills/skills.component';
 import { ResumeComponent } from "./about/resume/resume.component";
 import { AlertComponent } from './contact/alert-component/alert/alert.component';
@@ -23,7 +20,6 @@ import { AchivementsComponent } from './about/achivements/achivements.component'
     ContactComponent,
     TimelineComponent,
     HomeComponent,
-    LandingPageComponent,
     SkillsComponent,
     ResumeComponent,
     AlertComponent,
@@ -32,8 +28,7 @@ import { AchivementsComponent } from './about/achivements/achivements.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    appRoutingModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
